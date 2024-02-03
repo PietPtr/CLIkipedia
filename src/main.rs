c1776636f85f72e5898eee542de58b41eed33d65
Pieter Staal <pieterstaal@outlook.com>
2024-02-03 10:06:02 +0100

    Add unformatted HTML

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,25 @@ mod parser;
 mod wikipedia;
 
 use parser::HtmlParser;
-use std::error::Error;
+use std::{error::Error, fs};
 use wikipedia::Wikipedia;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let wikipedia = Wikipedia::new();
     let html = wikipedia.random_page().await?;
-    dbg!(HtmlParser::parse_page(html));
+    println!("{}", &html);
+    return Ok(());
+
+    let filename = "test.html";
+    match fs::read_to_string(filename) {
+        Ok(contents) => {
+            dbg!(HtmlParser::parse_page(contents));
+        }
+        Err(e) => {
+            println!("Error reading file: {}", e);
+        }
+    }
 
     Ok(())
 }
